32a0278da978234ca6dfbcc307622a6f1b12cc92
jmmontanari <joshua.montanari@gmail.com>
2021-01-10 18:53:03 -0500

    First Commit - Mac

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Components/Header/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>BNW Mobile</Text>
       <StatusBar style="auto" />
     </View>
---
+++ Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Header = () => {
+    return (
+        <View>
+            <Text>This will be the header</Text>
+        </View>
+    )
+}
+
+export default Header
